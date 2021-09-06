6bfcbdf2cd257ecb4f06cee684b63f515001bf67
Carlosviin <carlosvin26@gmail.com>
2021-09-06 17:04:01 -0300

    Foi criado um arquivo JavaScript

---
+++ Aula1.js
@@ -0,0 +1,13 @@
+Nome = "José Carlos"
+Idade = 39
+Telefone = "9999-9999"
+Endereço = "Avenina Sei lá onde"
+Número_da_casa = 557
+Cidade = "Miami"
+Cep = "04557-090"
+CPF = "005.445.886-69"
+Rg = "97.486.486-5"
+
+
+console.log("O nome do contato é " + Nome + " ele tem " + Idade + " anos de idade, o numero de contato dele é " + Telefone + " mora no endereço " + Endereço + "com o numero " + Número_da_casa + " da residência, na cidade " + Cidade + "com o cep " + Cep)
+console.log("Numero de cpf " + CPF + " e rg " + Rg)
